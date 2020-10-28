a769d0a233e6bb198ec1dec6ffd2e9eee7c96ac0
louisejungstedt <62991720+louisejungstedt@users.noreply.github.com>
2020-10-28 10:34:24 +0100

    Add files via upload

---
+++ projekt-ny.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace BuisnessLayer
+{
+    public class Podcast : ITitel
+    {
+        public string Url { get; set; }
+        public string Kategori { get; set; }
+        public string Uppdateringsfrekvens { get; set; }
+        public string AntalAvsnitt { get; set; }
+        public string Titel { get; set; }
+
+        public List<Avsnitt> AvsnittLista = new List<Avsnitt>();
+
+        [XmlIgnore]
+        public Timer uppdateringsTimer = new Timer();
+    }
+
+  
+}
+
+
+
+
